cf121e244460ab4a7fdffa1689d6386fb02d1150
MrCheseBall <sekhaship960@gmail.com>
2024-07-01 11:12:56 +0530

    done

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './index.css';
+
+
+import Header from "./Header"
+import AddContact from "./AddContact"
+import ContactCard from "./ContactCard"
+import ContactList from "./ContactList"
+function App() {
+  return (
+   <div>
+    <Header/>
+    {/* <AddContact/>
+    <ContactList/> */}
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Header = () => {
+    return ( 
+    <div>
+        <div className="ui fixed menu">
+            <div className="ui container center">
+            <h1 class="text-red-500">
+            Hello worldo!
+            </h1>
+            </div>
+        </div>
+    </div> 
+    );
+}
+ 
+export default Header;
